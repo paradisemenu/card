2d34af544ce17f4387bac5281c93407a6d59edbc
paradisemenu <108617564+paradisemenu@users.noreply.github.com>
2022-08-21 20:38:43 +0530

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -1,13 +1,8 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close'),
-      category1 = document.getElementById('nav-category1'),
-      category2 = document.getElementById('nav-category2'),
-      category3 = document.getElementById('nav-category3'),
-      category4 = document.getElementById('nav-category4'),
-      category5 = document.getElementById('nav-category5'),
-      category6 = document.getElementById('nav-category6')
+      navClose = document.getElementById('nav-close')
+   
 
 /*===== MENU SHOW =====*/
 /* Validate If Constant Exists */
@@ -18,44 +13,4 @@ if(navToggle) {
     })
 }
 
-/*===== MENU HIDDEN =====*/
-/* Validate If Constant Exists */
-if(navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
-}
-
-if(category1) {
-    category1.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
-}
-
-if(category2) {
-    category2.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
-}
-if(category3) {
-    category3.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
-}
-if(category4) {
-    category4.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
-}
-if(category5) {
-    category5.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
-}
-
-if(category6) {
-    category6.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
-}
 
