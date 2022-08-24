a9d07003d70745dc8f96fed8ce2ac1f6a9c71fb5
paradisemenu <108617564+paradisemenu@users.noreply.github.com>
2022-08-24 15:17:48 +0530

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -14,3 +14,7 @@ if(navToggle) {
 }
 
 
+window.addEventListener("load", function () {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden"; // class "loader hidden"
+  });
